f27c0f0e1c73ff07cef3423aeb74e1d8191adfe1
TTRSQ <tst.ttrsq.123@gmail.com>
2024-04-11 02:00:12 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import * as ff from "@google-cloud/functions-framework";
 import { Datastore } from "@google-cloud/datastore";
 
-const datastore = new Datastore();
+const datastore = new Datastore({
+  projectId: "hoge",
+  namespace: "fuga",
+});
 
 ff.http("SwitchBotEvent", async (req: ff.Request, res: ff.Response) => {
-  const key = datastore.key("SwitchBotEvent");
-  const body = req.body.context;
+  const key = datastore.key(req.body.eventType);
   const row = {
     key,
     data: {
-      deviceId: body.deviceMac,
+      eventVersion: req.body.eventVersion,
       createdAt: new Date().getTime(),
-      ...req.body,
+      ...req.body.context,
     },
   };
   const dbres = await datastore.save(row);
